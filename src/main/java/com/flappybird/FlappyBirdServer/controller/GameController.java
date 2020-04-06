f693f19df0d8ea05041d70607244738a1dcbcafb
Gaurav Sharma <gaurasha.gs@gmail.com>
2020-04-06 17:39:07 +0530

    removing delay

--- src/main/java/com/flappybird/FlappyBirdServer/controller/GameController.java
+++ src/main/java/com/flappybird/FlappyBirdServer/controller/GameController.java
@@ -15,7 +15,7 @@ public class GameController {
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(1000); // simulated delay
+
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 }
